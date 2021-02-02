bd9b29c1f30f7ce8a506c35035eb5ef3bf92ea0a
uo269736 <uo269736@uniovi.es>
2021-02-02 13:14:49 +0100

    SDI-2021-803-1.8-MVC, Modelo Vista Controlador.

--- sdi2021-803-lab-jee/src/com/uniovi/sdi/ServletCarrito.java
+++ sdi2021-803-lab-jee/src/com/uniovi/sdi/ServletCarrito.java
@@ -34,7 +34,7 @@ public class ServletCarrito extends HttpServlet {
 			throws ServletException, IOException {
 		HttpSession session = request.getSession();
 		ConcurrentHashMap<String, Integer> carrito = (ConcurrentHashMap<String, Integer>) request.getSession().getAttribute("carrito");
-//					No	hay	carrito,	creamos	uno	y	lo	insertamos	en	sesión
+//			No	hay	carrito,	creamos	uno	y	lo	insertamos	en	sesión
 		if (carrito == null) {
 			carrito = new ConcurrentHashMap<String, Integer>();
 			request.getSession().setAttribute("carrito", carrito);
@@ -43,14 +43,9 @@ public class ServletCarrito extends HttpServlet {
 		if (producto != null) {
 			insertarEnCarrito(carrito, producto);
 		}
-		response.setCharacterEncoding("UTF-8");
-		response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
-		out.println("<HTML>");
-		out.println("<HEAD><TITLE>Tienda	SDI:	carrito</TITLE></HEAD>");
-		out.println("<BODY>");
-		out.println(carritoEnHTML(carrito) + "<br>");
-		out.println("<a	href=\"index.jsp\">Volver</a></BODY></HTML>");
+//			Retornar	la	vista	con	parámetro	"carrito"
+		request.setAttribute("paresCarrito", carrito);
+		getServletContext().getRequestDispatcher("/vista-carrito.jsp").forward(request, response);
 	}
 
 	/**
@@ -61,21 +56,21 @@ public class ServletCarrito extends HttpServlet {
 			throws ServletException, IOException {
 		doGet(request, response);
 	}
-	
-	private void insertarEnCarrito(Map<String,Integer> carrito,String claveProducto) {
-		if(carrito.get(claveProducto)==null)
-			carrito.put(claveProducto,new Integer(1));
+
+	private void insertarEnCarrito(Map<String, Integer> carrito, String claveProducto) {
+		if (carrito.get(claveProducto) == null)
+			carrito.put(claveProducto, new Integer(1));
 		else {
-			int numeroArticulos=(Integer) carrito.get(claveProducto).intValue();
-			carrito.put(claveProducto,new Integer(numeroArticulos+1));
+			int numeroArticulos = (Integer) carrito.get(claveProducto).intValue();
+			carrito.put(claveProducto, new Integer(numeroArticulos + 1));
 		}
 	}
-	
-	private String carritoEnHTML(Map<String,Integer> carrito) {
-		String carritoEnHTML="";
-		
-		for(String key:carrito.keySet())
-			carritoEnHTML+="<p>["+key+"], "+carrito.get(key)+" unidades</p>";
+
+	private String carritoEnHTML(Map<String, Integer> carrito) {
+		String carritoEnHTML = "";
+
+		for (String key : carrito.keySet())
+			carritoEnHTML += "<p>[" + key + "], " + carrito.get(key) + " unidades</p>";
 		return carritoEnHTML;
 	}
 
