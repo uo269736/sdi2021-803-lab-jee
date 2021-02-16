507406787cf0794264e2688029d34514c118171d
uo269736 <uo269736@uniovi.es>
2021-02-16 12:50:15 +0100

    SDI–session2-Ejercicio-complementario3

--- sdi2021-803-spring/src/main/java/com/uniovi/controllers/ProfessorController.java
+++ sdi2021-803-spring/src/main/java/com/uniovi/controllers/ProfessorController.java
@@ -1,50 +1,62 @@
 package com.uniovi.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.uniovi.entities.Professor;
 import com.uniovi.services.ProfessorService;
 
-@RestController
+@Controller
 public class ProfessorController {
 	
 	@Autowired //Inyectar el servicio
 	private ProfessorService professorService;
 	
+	@RequestMapping(value = "/professor/add")
+	public String getProfessor() {
+		return "professor/add";
+	}
+	
+	@RequestMapping("/professor/list")
+	public String getList(Model model) {
+		model.addAttribute("professorList", professorService.getProfessors());
+		return "professor/list";
+	}
+	
 	@RequestMapping(value = "/professor/edit/{id}", method = RequestMethod.POST)
 	public String setEdit(Model model, @PathVariable Long id, @ModelAttribute Professor professor) {
 		professor.setId(id);
 		professorService.addProfessor(professor);
-		return "Edit professor with id:" + id;
+		return "redirect:/professor/details/" + id;
 	}
 	
 	@RequestMapping(value = "/professor/edit/{id}")
 	public String getEdit(Model model, @PathVariable Long id) {
 		model.addAttribute("professor", professorService.getProfessor(id));
-		return "Editing";
+		return "professor/edit";
 	}
 
-	@RequestMapping(value="/profesor/add",method=RequestMethod.POST)
+	@RequestMapping(value="/professor/add",method=RequestMethod.POST)
 	public String setProfesor(@ModelAttribute Professor p) {
 		professorService.addProfessor(p);
-		return "Added: "+" "+p.getId()+" "+p.getDni()+", "+p.getNombre()+" "+p.getApellidos()+", "+p.getCategoria();
+		return "redirect:/professor/list";
 	}
 
 	@RequestMapping("/professor/details/{id}")
-	public String getDetail(@PathVariable Long id) {
-		return professorService.getProfessor(id).toString();
+	public String getDetail(Model model,@PathVariable Long id) {
+		model.addAttribute("professor",professorService.getProfessor(id));
+		return "professor/details";
 	}
 	
-	@RequestMapping("/professor/delete/{id} ")
+	@RequestMapping("/professor/delete/{id}")
 	public String deleteProfessor(@PathVariable Long id) {
 		professorService.deleteProfessor(id);
-		return "Deleting Professor";
+		return "redirect:/professor/list";
 	}
 
 }
--- sdi2021-803-spring/src/main/java/com/uniovi/services/ProfessorService.java
+++ sdi2021-803-spring/src/main/java/com/uniovi/services/ProfessorService.java
@@ -1,16 +1,27 @@
 package com.uniovi.services;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.uniovi.entities.Professor;
 import com.uniovi.repositories.ProfessorRepository;
 
+@Service
 public class ProfessorService {
 
 	@Autowired
 	private ProfessorRepository professorRepository; 
 	
 
+	public List<Professor> getProfessors() {
+		List<Professor> professors = new ArrayList<Professor>();
+		professorRepository.findAll().forEach(professors::add);
+		return professors;
+	}
+	
 	public Professor getProfessor(Long id) {
 		return professorRepository.findById(id).get();
 	}
