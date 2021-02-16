220f416bd15a67560bf3fc267adbf6d59c1c03f9
uo269736 <uo269736@uniovi.es>
2021-02-16 12:04:18 +0100

    SDI–session2-Ejercicio-complementario2

--- sdi2021-803-spring/src/main/java/com/uniovi/controllers/ProfessorController.java
+++ sdi2021-803-spring/src/main/java/com/uniovi/controllers/ProfessorController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.uniovi.entities.Mark;
 import com.uniovi.entities.Professor;
 import com.uniovi.services.ProfessorService;
 
--- sdi2021-803-spring/src/main/java/com/uniovi/services/ProfessorService.java
+++ sdi2021-803-spring/src/main/java/com/uniovi/services/ProfessorService.java
@@ -1,36 +1,25 @@
 package com.uniovi.services;
 
-import java.util.LinkedList;
-import java.util.List;
-
-import javax.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.uniovi.entities.Professor;
+import com.uniovi.repositories.ProfessorRepository;
 
 public class ProfessorService {
 
-	private List<Professor> professorList = new LinkedList<Professor>();
-
-	@PostConstruct
-	public void init() {
-		professorList.add(new Professor(1L, "14678655T", "Manolo", "Lopez Garcia", "Ciencias"));
-		professorList.add(new Professor(2L, "98765678D", "Maria", "Marcos Arrende", "Ciencias"));
-	}
+	@Autowired
+	private ProfessorRepository professorRepository; 
+	
 
 	public Professor getProfessor(Long id) {
-		return professorList.stream().filter(professor -> professor.getId().equals(id)).findFirst().get();
+		return professorRepository.findById(id).get();
 	}
 
 	public void addProfessor(Professor professor) {
-		// Si en Id es null le asignamos el ultimo + 1 de la lista
-		if (professor.getId() == null) {
-			professor.setId(professorList.get(professorList.size() - 1).getId() + 1);
-		}
-
-		professorList.add(professor);
+		professorRepository.save(professor);
 	}
 
 	public void deleteProfessor(Long id) {
-		professorList.removeIf(professor -> professor.getId().equals(id));
+		professorRepository.deleteById(id);
 	}
 }
