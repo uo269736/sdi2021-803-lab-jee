6215f82afeff5fa15d855f7e607393c8c5ee966e
uo269736 <uo269736@uniovi.es>
2021-02-24 17:45:14 +0100

    SDI-2021-803-4.4-Consulta.

--- sdi2021-803-spring/src/main/java/com/uniovi/controllers/MarksController.java
+++ sdi2021-803-spring/src/main/java/com/uniovi/controllers/MarksController.java
@@ -1,5 +1,7 @@
 package com.uniovi.controllers;
 
+import java.security.Principal;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.uniovi.entities.Mark;
+import com.uniovi.entities.User;
 import com.uniovi.services.MarksService;
 import com.uniovi.services.UsersService;
 import com.uniovi.validators.MarkAddFormValidator;
@@ -33,8 +36,10 @@ public class MarksController {
 	private MarkAddFormValidator markAddFormValidator;
 
 	@RequestMapping("/mark/list")
-	public String getList(Model model) {
-		model.addAttribute("markList", marksService.getMarks());
+	public String getList(Model model, Principal principal) {
+		String dni = principal.getName(); // DNI es el name de la autenticación
+		User user = usersService.getUserByDni(dni);
+		model.addAttribute("markList", marksService.getMarksForUser(user));
 		return "mark/list";
 	}
 
@@ -86,8 +91,10 @@ public class MarksController {
 	}
 
 	@RequestMapping("/mark/list/update")
-	public String updateList(Model model) {
-		model.addAttribute("markList", marksService.getMarks());
+	public String updateList(Model model, Principal principal) {
+		String dni = principal.getName(); // DNI es el name de la autenticación
+		User user = usersService.getUserByDni(dni);
+		model.addAttribute("markList", marksService.getMarksForUser(user));
 		return "mark/list :: tableMarks";
 	}
 
--- sdi2021-803-spring/src/main/java/com/uniovi/repositories/MarksRepository.java
+++ sdi2021-803-spring/src/main/java/com/uniovi/repositories/MarksRepository.java
@@ -1,5 +1,7 @@
 package com.uniovi.repositories;
 
+import java.util.List;
+
 import javax.transaction.Transactional;
 
 import org.springframework.data.jpa.repository.Modifying;
@@ -7,6 +9,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.uniovi.entities.Mark;
+import com.uniovi.entities.User;
 
 public interface MarksRepository extends CrudRepository<Mark, Long> {
 	
@@ -14,5 +17,9 @@ public interface MarksRepository extends CrudRepository<Mark, Long> {
 	@Transactional
 	@Query("UPDATE Mark SET resend = ?1 WHERE id = ?2")
 	void updateResend(Boolean resend, Long id);
+	
+	@Query("SELECT r FROM Mark r WHERE r.user = ?1 ORDER BY r.id ASC ")
+	List<Mark> findAllByUser(User user);
+
 
 }
