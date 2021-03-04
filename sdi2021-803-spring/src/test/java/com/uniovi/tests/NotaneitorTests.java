7aace7676ca40c283debf0d841d826a7ed8c42b4
uo269736 <uo269736@uniovi.es>
2021-03-04 18:11:14 +0100

    SDI-2021-803-5.4-Casos Uso VistaHome.

--- sdi2021-803-spring/src/test/java/com/uniovi/tests/NotaneitorTests.java
+++ sdi2021-803-spring/src/test/java/com/uniovi/tests/NotaneitorTests.java
@@ -1,7 +1,5 @@
 package com.uniovi.tests;
 
-import static org.junit.Assert.fail;
-
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -12,14 +10,18 @@ import org.junit.runners.MethodSorters;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import com.uniovi.tests.pageobjects.PO_HomeView;
+import com.uniovi.tests.pageobjects.PO_Properties;
+
 //Ordenamos las pruebas por el nombre del método
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 
 public class NotaneitorTests {
-	// En Windows (Debe ser la versión 65.0.1 y desactivar las actualizacioens automáticas)):
+	// En Windows (Debe ser la versión 65.0.1 y desactivar las actualizacioens
+	// automáticas)):
 	static String PathFirefox65 = "C:\\Program Files\\Mozilla Firefox\\firefox.exe";
 	static String Geckdriver024 = "C:\\Users\\MiguelUni\\Desktop\\TrabajoUniversidadMiguel\\Tercero\\SDI\\Sesion 5\\PL-SDI-Sesión5-material\\geckodriver024win64.exe";
-	
+
 	static WebDriver driver = getDriver(PathFirefox65, Geckdriver024);
 	static String URL = "http://localhost:8090";
 
@@ -54,8 +56,32 @@ public class NotaneitorTests {
 		driver.quit();
 	}
 
+	// PR01. Acceder a la página principal /
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void PR01() {
+		PO_HomeView.checkWelcome(driver, PO_Properties.getSPANISH());
 	}
+
+	// PR02. OPción de navegación. Pinchar en el enlace Registro en la página home
+	@Test
+	public void PR02() {
+		PO_HomeView.clickOption(driver, "signup", "class", "btn btn-primary");
+	}
+
+	// PR03. OPción de navegación. Pinchar en el enlace Identificate en la página
+	// home
+	@Test
+	public void PR03() {
+		PO_HomeView.clickOption(driver, "login", "class", "btn btn-primary");
+	}
+
+	// PR04. OPción de navegación. Cambio de idioma de Español a Ingles y vuelta a
+	// Español
+	@Test
+	public void PR04() {
+		PO_HomeView.checkChangeIdiom(driver, "btnSpanish", "btnEnglish", PO_Properties.getSPANISH(),
+				PO_Properties.getENGLISH());
+		// SeleniumUtils.esperarSegundos(driver, 2);
+	}
+
 }
